8835b32409abb08a0bb32aec55d3677271c8a9ff
Alexandr Wang <alexandrwang@dhcp-18-189-126-131.dyn.MIT.EDU>
2015-11-28 16:28:13 -0500

    fix bug oops

--- fer2013/cnn/network.py
+++ fer2013/cnn/network.py
@@ -210,7 +210,7 @@ def inference(images):
   #
   # conv1
   with tf.variable_scope('conv1') as scope:
-    kernel = _variable_with_weight_decay('weights', shape=[5, 5, 3, 64],
+    kernel = _variable_with_weight_decay('weights', shape=[5, 5, 1, 64],
                                          stddev=1e-4, wd=0.0)
     conv = tf.nn.conv2d(images, kernel, [1, 1, 1, 1], padding='SAME')
     biases = _variable_on_cpu('biases', [64], tf.constant_initializer(0.0))
